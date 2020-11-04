ffd906d4ee24104f9c673c8e9e6f6f4faaa7f29e
Asrock <esvalcheva@gmail.com>
2020-11-04 11:29:53 +0200

    Date

--- src/ed/school/Datetest.java
+++ src/ed/school/Datetest.java
@@ -3,8 +3,16 @@ package ed.school;
 public class Datetest {
 
 	public static void main(String[] args) {
-		System.out.println("Hallo");
+	
+		Date d = new Date(21, 22, 15, 3, 3, 2021);
+		Date d1 = new Date(33, 21, 3, 12, 8, 2011);
+		
+		System.out.println(d);
+		System.out.println(d1);
+		System.out.println(d1.compareTo(d));
+		System.out.println(d1.LeapYear());
+		
+	
 
-	}
-
+}
 }
